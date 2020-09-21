9aadd18d271c39e13667c2cb7c508653821d2288
mawilson1234 <mawilson@linguist.umass.edu>
2020-09-21 12:54:29 -0400

    Initial commti

---
+++ data_includes/main.js
@@ -0,0 +1,93 @@
+PennController.ResetPrefix(null) // Shorten command names (keep this line here)
+PennController.DebugOff()
+
+//var counterOverride = 1;
+PennController.SetCounter( "setcounter" );
+
+Sequence("setcounter", "consent", "intro", "instruction", randomize("trial_prac"), "instruction2", /*rshuffle("trial_agr-att", "trial_that", "trial_experiencer", "trial_fillers", "trial_whif"), "feedback",*/ SendResults(), "bye")
+
+newTrial( "consent" ,
+    newText("CONSENT GOES HERE")
+        .settings.css("margin","50px")
+        .print()
+    ,
+
+    newButton("Next","Next")
+        .center()
+        .settings.css("font-size", "2em")
+        .settings.css("margin", "80px")
+        .settings.size(500, 48)
+        .print()
+        .wait()
+)
+
+newTrial("intro" ,
+    newText("INSTRUCTIONS GO HERE")
+        .settings.css("margin","50px")
+        .print()
+    ,
+
+    newTextInput("ProlificID")
+        .before(newText("ID", "Your Prolific ID:<p>").settings.css("margin", "50px"))
+        .settings.css('width', '30%')
+        .settings.css('margin', 'auto')
+        .print()
+        .log()
+    ,
+    
+    newButton("Next","Next")
+        .center()
+        .settings.css("font-size", "2em")
+        .settings.css("margin", "80px")
+        .settings.size(500, 48)
+        .print()
+        .wait()
+)
+
+newTrial("instruction",
+    newText("Instr", "INSTRUCTIONS GO HERE")
+        .settings.css("margin", "80px")
+        .print()
+    ,
+
+    newButton("Click","Click here to begin practice trials!")
+        .center()
+        .settings.css("margin", "80px")
+        .settings.size(300, 16)
+        .print()
+        .wait()
+)
+
+PennController.Template("practice.csv", variable => ["trial_prac",
+        "DashedSentence", {s: variable.Sentence, mode: "speeded acceptability", display: "in place",
+                           q: 'Was the sentence grammatical?', as: [['f', 'Yes'], ['j', 'No']],
+                           randomOrder: false, presentHorizontally: true, blankText: '+',
+                           wordTime: 225, wordPauseTime: 100, timeout: 2000}
+    ]
+)
+
+
+newTrial("instruction2",
+    newText("Instr2", "POST-PRACTICE INSTRUCTIONS GO HERE")
+        .settings.css("margin", "80px")
+        .print()
+    ,
+
+    newButton("Click","Click here to begin the experiment")
+        .center()
+        .settings.css("margin", "80px")
+        .settings.size(300, 16)
+        .print()
+        .wait()
+)
+
+// Spaces and linebreaks don't matter to the script: we've only been using them for the sake of readability
+newTrial("bye" ,
+    newText("Thank you for your participation! Please go to the following web page to verify your participation: <a href='https://app.prolific.co/submissions/complete?cc=728AA2CF'> https://app.prolific.co/submissions/complete?cc=728AA2CF</a>.")
+        .print(),
+        
+    newButton()
+        .wait()  // Wait for a click on a non-displayed button = wait here forever
+)
+.setOption("countsForProgressBar" , false)
+// Make sure the progress bar is full upon reaching this last (non-)trial
